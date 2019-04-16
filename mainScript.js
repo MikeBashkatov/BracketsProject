7c3a38443a62f8f7302a8fed4ca896b5231fc3f1
Mike <wds.bms@gmail.com>
2019-04-15 23:59:51 -0300

    Brackets appear
    Added weighClasses select + brackets depend on the weigh class

--- mainScript.js
+++ mainScript.js
@@ -44,69 +44,94 @@ window.onload = function(){
     startButton.onclick = function(){
         let select = $('weightList');
         let inputs = document.getElementsByClassName("weight");
+        let main = $('page-2');
 
-        // removing ALL options so we can insert new ones
-        for(let i = select.options.length - 1 ; i > 0 ; i--)
-        {
-            select.options.remove(i);
+        // CHECKING IF WE HAVE AT LEAST ONE WEIGHT cLASS ENTERED
+
+        if(inputs.length >= 1 && inputs[0].value !== ""){
+
+
+            // removing ALL options so we can insert new ones
+            for(let i = select.options.length - 1 ; i > 0 ; i--)
+            {
+                select.options.remove(i);
+            }
+
+            // Inserting new options from input elements
+            for (let i = 0; i < inputs.length; i++) {
+
+                let itemText = inputs[i].value;
+
+                if (itemText.match(regEx)) {  // check that all fields are filled with content
+                    let option = document.createElement('option');
+                    option.textContent = itemText;
+                    option.value = itemText;
+                    option.id = itemText;
+
+                    select.appendChild(option);
+
+                    //alert(`Class ${option.value} added successfully`);  - for checking purposes
+                    let flag = false;
+                    for (let i = 0; i < main.childNodes.length; i++){ // take children inside PAGE-2 and check for duplicates in divs ID's (if div already there we don't need to create extra one)
+                        if (main.childNodes[i].id == itemText) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag !== true) {
+                    // creating inner div with inputs + assigning them IDs same as text in options for identification
+                    let innerDiv = document.createElement('DIV');
+                    innerDiv.id = itemText;
+                    innerDiv.className = 'hiddenDiv';
+
+                    let participantDiv = document.createElement('DIV');
+                    participantDiv.className = 'participants';
+
+                    let createFirstNameInput = document.createElement('input');
+                    createFirstNameInput.placeholder = 'First Name';
+                    createFirstNameInput.className = 'participantInfo';
+
+                    let createLastNameInput = document.createElement('input');
+                    createLastNameInput.placeholder = 'Last Name';
+                    createLastNameInput.className = 'participantInfo';
+
+                    let createCityInput = document.createElement('input');
+                    createCityInput.placeholder = 'City';
+                    createCityInput.className = 'participantInfo';
+
+                    let createProvinceInput = document.createElement('input');
+                    createProvinceInput.placeholder = 'Province';
+                    createProvinceInput.className = 'participantInfo';
+
+                    let deleteElement = document.createElement('I');
+                    deleteElement.className = 'fas fa-times-circle';
+
+                    main.appendChild(innerDiv);
+
+
+                        let participantsInfo = document.querySelectorAll('#page-2 > .hiddenDiv'); // cheking if selector already have a div created inside
+                        for (let i = 0; i < participantsInfo.length; i++) {
+                            if (participantsInfo[i].innerHTML == '') {
+                                innerDiv.appendChild(participantDiv);
+                                participantDiv.append(createFirstNameInput, createLastNameInput, createCityInput, createProvinceInput, deleteElement);
+                            }
+                        }
+                    }
+                }
+                else {
+                        alert(errorMsg);
+                        break;
+                    }
+
+            }// END OF FOR LOOP
+
+
+            $('main').className = 'hiddenDiv';
+            $('page-2').className = 'showDiv';
         }
-
-        // Inserting new options from input elements
-        for (let i = 0; i < inputs.length; i++) {
-
-            let itemText = inputs[i].value;
-
-            if(itemText.match(regEx)){  // check that all fields are filled with content
-                let option = document.createElement('option');
-                option.textContent = itemText;
-                option.value = itemText;
-                option.id = itemText;
-
-                select.appendChild(option);
-
-                //alert(`Class ${option.value} added successfully`);  - for checking purposes
-
-                // creating inner div with inputs + assigning them IDs same as text in options for identification
-                let innerDiv = document.createElement('DIV');
-                innerDiv.id = itemText;
-                innerDiv.className = 'hiddenDiv';
-
-                let participantDiv = document.createElement('DIV');
-                participantDiv.className = 'participants';
-
-                let createFirstNameInput = document.createElement('input');
-                createFirstNameInput.placeholder = 'First Name';
-                createFirstNameInput.className = 'participantInfo';
-
-                let createLastNameInput = document.createElement('input');
-                createLastNameInput.placeholder = 'Last Name';
-                createLastNameInput.className = 'participantInfo';
-
-                let createCityInput = document.createElement('input');
-                createCityInput.placeholder = 'City';
-                createCityInput.className = 'participantInfo';
-
-                let createProvinceInput = document.createElement('input');
-                createProvinceInput.placeholder = 'Province';
-                createProvinceInput.className = 'participantInfo';
-
-                let deleteElement = document.createElement('I');
-                deleteElement.className = 'fas fa-times-circle';
-
-                let main = $('page-2');
-                main.appendChild(innerDiv);
-                innerDiv.appendChild(participantDiv);
-                participantDiv.append(createFirstNameInput, createLastNameInput, createCityInput, createProvinceInput, deleteElement);
-
-            }
-            else {
-                alert(errorMsg);
-                break;
-            }
-        } // END OF FOR LOOP
-
-        $('main').className = 'hiddenDiv';
-        $('page-2').className = 'showDiv';
+        else{
+            alert("Please enter at least one weight class");
+        }
     }
 
 // BACK button funtionality
@@ -165,6 +190,89 @@ window.onload = function(){
             };
         });
     }; // END addParticipants FUNCTION
+
+
+    // Creating select box to choose weight class bracket!
+
+    let buildBrackets = $('StartTournament');
+
+    // creating brackets accordign to the weighclasses
+
+    function buildBracket(){
+        let select = $('bracketsList');
+        let bracketMain = document.createElement('DIV');
+        select.parentNode.insertBefore(bracketMain, select.nextSibling); //inserting main Brackets DIV right after the Brackets Select
+
+        let inputs = document.getElementsByClassName('weight');
+
+        // CHECKING IF WE HAVE AT LEAST ONE WEIGHT cLASS ENTERED
+
+        if(inputs.length >= 1 && inputs[0].value !== ""){
+
+
+            // removing ALL options so we can insert new ones
+            for(let i = select.options.length - 1 ; i > 0 ; i--)
+            {
+                select.options.remove(i);
+            }
+
+            // Inserting new options from input elements
+            for (let i = 0; i < inputs.length; i++) {
+
+                let itemText = inputs[i].value;
+
+                    let option = document.createElement('option');
+                    option.textContent = itemText;
+                    option.value = itemText;
+                    option.id = itemText;
+
+                    select.appendChild(option);
+
+            }
+            //insert brackets into newely created divs
+            let options = select.options.item;
+            for(let i = select.options.length - 1 ; i > 0 ; i--)
+            {
+                let itemText = options[i].text;
+
+                let bracketInner = document.createElement('DIV');
+                bracketInner.classList = itemText;
+                bracketMain.appendChild(bracketInner);
+
+
+                var bigData = {
+                    teams : [
+                        ["Team 1",  "Team 2" ],
+                        ["Team 3",  "Team 4" ],
+                        ["Team 5",  "Team 6" ],
+                        ["Team 7",  "Team 8" ],
+                        ["Team 9",  "Team 10"],
+                        ["Team 11", "Team 12"],
+                        ["Team 13", "Team 14"],
+                        ["Team 15", "Team 16"]
+                    ],
+                    results : [[[[]]], [], []] // minimal array to initialize DE bracket
+                }
+
+                $(function() { $(`${itemText}`).bracket(
+                    {
+                        init: bigData,
+                        save: function(){},
+                        disableToolbar: true,
+                        disableTeamEdit: true,
+                        skipConsolationRound: true,
+                        centerConnectors: true
+                    }
+                )
+                })
+            }
+        }
+    }
+
+    buildBrackets.onclick = buildBracket;
+
+
+
 
 
 // const ul = document.querySelector('ul');
