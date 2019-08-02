dc0b3dbeeec0000b8f0fdc527cc594bc2002f2b2
Mike <wds.bms@gmail.com>
2019-08-02 17:56:48 -0300

    single and doble elimination works uhu !

--- BracketScript.js
+++ BracketScript.js
@@ -1,26 +1,34 @@
+//Right now if you add only 2 participants brackets plugin does not want to accept it in DE version
 let buildBrackets = document.getElementById('StartTournament');
 let bracketInner; // DIV element where we store our bracket that has a class equals to the weight category
 let itemOption; // variable that stores weigh class value retrieved from Options of a select box
 let select = document.getElementById('bracketsList'); // variable that stores select box of hide-show brackets
-
+let selectElimType = document.getElementById('StartBrackets');
+let eliminationType;
 
 buildBrackets.addEventListener("click", brackets);
 
+selectElimType.addEventListener("click",  pick_elimination);
+
 select.addEventListener("change", showBrakets);
 
-function brackets(){
-    let hugeData = [];
-    let smallData = [];
-    let fNamelName = '';
-    let hiddenDivs = document.querySelectorAll('#page-2 > .hiddenDiv').length; // get the number of hidden divs on page2
-    let partisipantName;// contains all info about participants in a div #i
-    for (let i = 0; i < hiddenDivs; i++){
-        partisipantName = document.querySelectorAll(`.p${i} > .participants`);
-        for(let i = 0; i <= partisipantName.length; i++){
-            fNamelName = partisipantName[i].childNodes[0].nodeValue + ' ' + partisipantName[i].childNodes[1].nodeValue;
-            smallData.push(fNamelName);
-        }
+function pick_elimination(){
+    let selectedElimType = $("#PickElimination").val();
+    if(selectedElimType == 'Single elimination'){
+        eliminationType = [];
     }
+    else if(selectedElimType == 'Double elimination'){
+        eliminationType = [[[[]]], [], []];
+    }
+    else if(selectedElimType == 'Triple elimination'){
+        eliminationType = [];
+    }
+    else{
+        alert("Please select elimination type");
+    }
+}
+
+function brackets(){
 
     let bracketMain = document.createElement('DIV');
     bracketMain.classList.add('BracketsVisual')
@@ -52,45 +60,59 @@ function brackets(){
             select.appendChild(option);
 
         }
-        //insert brackets into newely created divs
-        for(let i = select.options.length - 1 ; i > 0 ; i--)
-        {
-            itemOption = select.options[i].innerText;
+    }
 
-            bracketInner = document.createElement('DIV');
-            bracketInner.classList.add(itemOption, 'hiddenDiv');
-            bracketMain.appendChild(bracketInner);
+    let hiddenDivs = document.querySelectorAll('#page-2 > .hiddenDiv').length; // get the number of hidden divs on page2
+    let partisipantName;// contains all info about participants in a div #i
+    var hugeData = [];
+    for (let i = 0; i < hiddenDivs; i++){
+        partisipantName = document.querySelectorAll(`.p${i} > .participants`);
 
-            var bigData = {
-                teams: [
-                    ["Team 1", "Team 2"],
-                    ["Team 3", "Team 4"],
-                    ["Team 5", "Team 6"],
-                    ["Team 7", "Team 8"],
-                    ["Team 9", "Team 10"],
-                    ["Team 11", "Team 12"],
-                    ["Team 13", "Team 14"],
-                    ["Team 15", "Team 16"]
-                ],
-                results: [[[[]]], [], []] // minimal array to initialize DE bracket
+        var mediumData = [];
+        let smallData = [];
+
+        for(let i = 0; i < partisipantName.length; i++){
+            if(i != 0 && i%2 == 0){ // here we are controlling the push so it is made every 2! itterations
+                mediumData.push(smallData);
+                smallData = [];
             }
+            smallData.push(partisipantName[i].childNodes[0].value + ' ' + partisipantName[i].childNodes[1].value);
 
-            $(function () {
-                $(`.${itemOption}`).bracket(
-                    {
-                        init: bigData,
-                        save: function () {
-                        },
-                        disableToolbar: true,
-                        disableTeamEdit: true,
-                        skipConsolationRound: true,
-                        centerConnectors: true
+            if((i+1)==partisipantName.length){ // here we checks if the loop got to the last element we are making last push to the hugeData array and end working on this weight class
+                if(smallData.length%2 != 0){ // here we checks if last small data class contains just one participant (number of participants might not be  even) we are adding NULL so brackets can work with it
+                    smallData.push(null);
+                }
+                mediumData.push(smallData);
+                smallData = [];
+
+                }
+            }
+            hugeData.push(mediumData);
+                    //insert brackets into newely created divs
+                    itemOption = select.options[i+1].innerText;
+
+                    bracketInner = document.createElement('DIV');
+                    bracketInner.classList.add(itemOption, 'hiddenDiv');
+                    bracketMain.appendChild(bracketInner);
+        
+                    var bigData = {
+                        teams: hugeData[i],
+                        results: eliminationType // minimal array to initialize DE bracket
                     }
-                )
-            })
-        }
-
-
+        
+                    $(function () {
+                        $(`.${itemOption}`).bracket(
+                            {
+                                init: bigData,
+                                save: function () {
+                                },
+                                disableToolbar: true,
+                                disableTeamEdit: true,
+                                skipConsolationRound: true,
+                                centerConnectors: true
+                            }
+                        )
+                    })
     }
 }
 
